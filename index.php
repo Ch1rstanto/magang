727a237f352da5ed76af9be555116fce6dfafe9a
unknown <erenkruger30@gmail.com>
2025-12-10 14:54:14 +0700

    ini inisial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login - Kelola Magang Politeknik Negeri Medan</title>
+    <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="card">
+            <h1 class="card-title">Login Pengelolaan Magang</h1>
+            <p class="card-subtitle">Sistem Magang Online Mahasiswa</p>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ readme.md
@@ -0,0 +1,3 @@
+Sistem Pengelolaan Magang Mahasiswa
+
+Project ini adalah sistem pengelolaan magang berbasis web untuk mahasiswa. Sistem ini memungkinkan admin dan mahasiswa 
